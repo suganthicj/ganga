a652d01ea963c8c8797f735e1f982abff870d532
suganthicj <52270901+suganthicj@users.noreply.github.com>
2019-07-15 21:27:28 -0700

    Create ganga.py

---
+++ ganga.py
@@ -0,0 +1,10 @@
+  
+list1 = [1, 2, 3, 4, 1, 1, 1, 4, 5] 
+  
+# Will print the index of '4' in list1 
+print(list1.index(4)) 
+  
+list2 = ['cat', 'bat', 'mat', 'cat', 'pet'] 
+  
+# Will print the index of 'cat' in list2  
+print(list2.index('cat')) 
